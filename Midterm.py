5482a79e7dca0c98715cfb0587a5063a825f012f
Alessio Ivancic <2474657@edu.vaniercollege.qc.ca>
2025-10-21 09:39:05 -0400

    Midterm Exam

--- Midterm.py
+++ Midterm.py
@@ -3,10 +3,26 @@ import math
 # Q1: Calculate the area of a circle
 def area_of_circle(radius):
     return 0.0
+    
+
+
+    
 
 # Q2: Hollow Right Triangle
 def hollow_right_triangle(n):
-    return ""
+    result = ""
+
+    if n < 4:
+        return("The triangle height should be at least 4.")
+
+    for i in range(n):
+        for j in range(n):
+            if j == i or i == n - 1 or j == 0:
+                return ("*" + "\n")
+            else:
+                return(" " + "\n")
+            
+    return result.rstrip
 
 # Q3: Inverted Pyramid
 def inverted_pyramid(n):
